9ac3a3b38ab5044d0738ceced5c6378adbdfb3a6
Tiuters <ands458@gmail.com>
2021-11-13 18:31:05 +0300

    Util for Hiber

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+import org.hibernate.metamodel.Metadata;
+import org.hibernate.metamodel.MetadataSources;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Util {
     private static String dbURL = "jdbc:mysql://localhost:3306/pp1_schema"; //?autoReconnect=true&useSSL=false
     private static String dbUsername = "root";
     private static String dbPassword = "ert45dfc67";
-
+    private static StandardServiceRegistry standardServiceRegistry;
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() {
         Connection connection = null;
@@ -18,8 +28,64 @@ public class Util {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
         return connection;
     }
+
+
+    static {
+        StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
+
+        Map<String, String> dbSettings = new HashMap<>();
+        dbSettings.put(Environment.URL, "jdbc:mysql://localhost:3306/sch_forhiber");
+        dbSettings.put(Environment.USER, "hiber");
+        dbSettings.put(Environment.PASS, "hiber");
+        dbSettings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+        dbSettings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+
+        registryBuilder.applySettings(dbSettings);
+        standardServiceRegistry = registryBuilder.build();
+        MetadataSources sources = new MetadataSources(standardServiceRegistry);
+        Metadata metadata = sources.getMetadataBuilder().build();
+        sessionFactory = metadata.getSessionFactoryBuilder().build();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
